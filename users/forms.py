d3c54d3ddc42992bc92b3960c954d781078d662b
EugeneZaharchenko <eugenezaharchenko@gmail.com>
2019-08-09 07:34:28 +0300

    UserCreateForm

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from .models import CustomUser
+# from .models import CustomUser
+from django.contrib.auth.models import User
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields
-
+        # model = CustomUser
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # fields = UserCreationForm.Meta.fields
+    def save(self, commit=True):
+        user = super(CustomUserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 class CustomUserChangeForm(UserChangeForm):
 
